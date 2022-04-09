70a6d425fbe40ae2297eb3edd3197a099c290517
uguryildirim <zaxscd1>
2022-04-10 02:58:28 +0300

    Ok.

--- index.php
+++ index.php
@@ -6,4 +6,3 @@ $regex = new App\Text\Replace();
 echo $regex->searchAndReplace("Ankara'nın Ankara ya faydası", 'ankara', 'samsun').PHP_EOL;
 echo $regex->searchAndReplace("Armut un iyisini ayılar yer", 'armut', 'elma').PHP_EOL;
 echo $regex->searchAndReplace("tatil'e Mersin e gittim", 'mersin', 'hatay').PHP_EOL;
-
